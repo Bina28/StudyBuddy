1a24de59aa9a007e24b6188861fcb40b261d1dc6
Bina <dyakovabina@gmail.com>
2025-10-05 14:55:15 +0200

    Add another migration

--- Persistence/Migrations/20251005122007_InitialCreate.Designer.cs
+++ Persistence/Migrations/20251005122007_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20251005121148_SecondMigration")]
-    partial class SecondMigration
+    [Migration("20251005122007_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
